1bcf6bf82f5f0dc53c077c83245b296a06a57b27
Gideon Obiasor <gideonoiasor@gmail.com>
2023-04-18 16:16:05 +0100

    Pascal triangle interview test

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,34 +8,26 @@ def pascal_triangle(n):
     """Returns list of lists of pascal's triangle integers
     """
 
-    if n <= 0:
-        return []
-    else:
-        # The coefficeint of any value of n is given by
-        # coeff = nCr = n! / (r! * (n-r)!)
-        list_of_list = []
-
-        def factorial(n):
-            """Helper function for calculating factorial"""
-            if n == 0:
-                return 1
-            elif n == 1:
-                return 1
-            return n * factorial(n-1)
-
+    list_of_list = []
+    if n > 0:
         for _ in range(n):
             pascal_list = []
 
-            # counter variable
-            r = 0
+            # Pascal coeff = nCr = n! / (r! * (n-r)!)
             # to accommodate index 0
             n = n - 1
-            for _ in range(n + 1):
+            for r in range(n + 1):
                 num = factorial(n) / (factorial(r) * factorial(n - r))
-                r += 1
-                pascal_list.append(int(num))
+                pascal_list.append(round(num))
             list_of_list.append(pascal_list)
 
-    # Return a reversed version of list of list to ensure to ensure
-    # increasing other as required
     return list_of_list[::-1]
+
+
+def factorial(n):
+    """Helper function for calculating factorial"""
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    return n * factorial(n-1)
