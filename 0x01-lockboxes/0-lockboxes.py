35a67691063c72e8dc78d11a87197554c9ad4c75
Gideon Obiasor <gideonoiasor@gmail.com>
2023-05-08 19:18:13 +0100

    Task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def canUnlockAll(boxes):
     num_boxes = len(boxes)
     available_keys = set([0])
@@ -10,14 +11,14 @@ def canUnlockAll(boxes):
         return False
     for key in boxes[0]:
         available_keys.add(key)
-    
+
     for box_id, box in enumerate(boxes):
         if box_id in available_keys:
             count = count + 1
             [available_keys.add(key) for key in box]
         else:
             no_keys.add(box_id)
-    for  box_id in no_keys:
+    for box_id in no_keys:
         if box_id in available_keys:
             [available_keys.add(key) for key in boxes[box_id]]
             count = count + 1
@@ -25,6 +26,6 @@ def canUnlockAll(boxes):
         return True
     else:
         return False
-    
+
 # boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
 # canUnlockAll(boxes)
